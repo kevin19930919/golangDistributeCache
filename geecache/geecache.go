9152b7fa7cf3eec7f2f0f80028a92379d04a880e
kevin_tsai <kevin19930919@gmail.com>
2022-10-24 13:29:14 +0800

    feat: add unit test for  group.get function

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -1,6 +1,10 @@
 package geecache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 type Group struct {
 	name      string
@@ -42,4 +46,36 @@ type GetterFunc func(key string) ([]byte, error)
 
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+// get value for a key from cache
+func (g *Group) Get(key string) ([]ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.Get(key); ok {
+		log.Println("cache hit!!!!")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.mainCache.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
